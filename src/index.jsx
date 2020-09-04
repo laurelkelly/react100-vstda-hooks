1e80835dc4661f586436afc25bc9663c73702d52
Laurel Kelly <laurellkelly@gmail.com>
2020-09-04 09:41:15 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 const DATA = [
-    { 
-        id: 'todo-0',
-        name: 'Eat',
-        completed: true,
-        priority: 2
-    },
-    { 
-        id: 'todo-1',
-        name: 'Sleep',
-        completed: false,
-        priority: 3
-    },
-    { 
-        id: 'todo-2',
-        name: 'Repeat',
-        completed: false,
-        priority: 4
-    }
+    // { 
+    //     id: 'todo-0',
+    //     name: 'Eat',
+    //     completed: true,
+    //     priority: 2
+    // },
+    // { 
+    //     id: 'todo-1',
+    //     name: 'Sleep',
+    //     completed: false,
+    //     priority: 3
+    // },
+    // { 
+    //     id: 'todo-2',
+    //     name: 'Repeat',
+    //     completed: false,
+    //     priority: 4
+    // }
 ];
 
 ReactDOM.render(<App tasks={DATA} />, document.getElementById('root'));
